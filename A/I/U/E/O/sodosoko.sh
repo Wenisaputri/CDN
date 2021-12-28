ad2c81ee9230bc5dd4674512300e8a22c3552f31
dp vpn <96043348+Wenisaputri@users.noreply.github.com>
2021-12-28 17:06:22 +0700

    Update sodosoko.sh

--- A/I/U/E/O/sodosoko.sh
+++ A/I/U/E/O/sodosoko.sh
@@ -88,6 +88,6 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 echo -e "Done Install Shadowsocks-libev" | lolcat
-figlet -f slant AFDHAN - NEZA | lolcat
+figlet -f slant DP VPN | lolcat
 sleep 1
 rm -f /root/sodosoko.sh
