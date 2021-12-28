a0f1af2d3a170c0e992b5954cb902ef7f36f909f
dp vpn <96043348+Wenisaputri@users.noreply.github.com>
2021-12-28 17:43:33 +0700

    Update wgo.sh

--- A/I/U/E/O/wgo.sh
+++ A/I/U/E/O/wgo.sh
@@ -22,7 +22,7 @@ if [[ -e /etc/wireguard/params ]]; then
 	exit 1
 fi
 
-echo -e "${Info} Wireguard VPS AutoScript by M AFDHAN - NezaVPN"
+echo -e "${Info} Wireguard VPS AutoScript by DP VPN"
 # Detect public IPv4 address and pre-fill for the user
 
 # Detect public interface and pre-fill for the user
